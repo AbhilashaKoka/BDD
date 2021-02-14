1fcf5fe683a3b75e9762d03287a98435cbdd90c7
10655479 <10655479@LTILPUN23200595.lntinfotech.com>
2021-02-14 22:05:49 +0530

    up

--- RCRC.Mobility.BDDFramework/src/test/java/cucumber/APITestContext.java
+++ RCRC.Mobility.BDDFramework/src/test/java/cucumber/APITestContext.java
@@ -4,6 +4,8 @@ import apiPageObjects.RestAssuredExtensionv2;
 import managers.FileReaderManager;
 //Test Context class to hold all the objects state.
 //Text Context class shall encompass all information your Steps files are using
+
+
 public class APITestContext {
 	//private final String BASE_URL = "https://bookstore.toolsqa.com";
 	// private final String USER_ID = "9b5f49ab-eea9-45f4-9d66-bcf56a531b85";
